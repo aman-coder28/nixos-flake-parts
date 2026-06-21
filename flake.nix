4a823e4001de549ed3194a1286c612ea0b680162
aman-coder28 <amancoder28@gmail.com>
2026-06-21 20:49:06 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,31 +15,23 @@
       url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-<<<<<<< HEAD
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-=======
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
->>>>>>> a2ea15c (Setup Home Manager)
   };
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake {
-      inherit inputs;
-<<<<<<< HEAD
-    } (inputs.import-tree ./modules);
-=======
-    } {
-      imports = [
-        # inputs.home-manager.flakeModules.home-manager
-        (inputs.import-tree ./modules)
-      ];
-    };
->>>>>>> a2ea15c (Setup Home Manager)
+    inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        imports = [
+          inputs.home-manager.flakeModules.home-manager
+          (inputs.import-tree ./modules)
+        ];
+      };
 }
