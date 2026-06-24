22be73cc34246a2d30de97372d62bc786f725e84
Aman Coder <115057435+aman-coder28@users.noreply.github.com>
2026-06-24 08:54:40 +0300

    Update vscode.nix

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -3,7 +3,16 @@
   flake.homeModules.vsCodeConfig = { ... }: {
     programs.vscode = {
       enable = true;
-      userSettings = builtins.fromJSON (builtins.readFile ./cursor-settings.json);
+
+      userSettings = (builtins.fromJSON (builtins.readFile ./cursor-settings.json)) // {
+        "workbench.colorTheme" = "Cursor Dark";
+        "workbench.activityBar.location" = "default";
+        "workbench.sideBar.location" = "left";
+        "editor.minimap.enabled" = false;
+        "window.commandCenter" = false;
+        "github.copilot.enable" = { "*" = false; };
+        "telemetry.telemetryLevel" = "off";
+      };
 
       mutableExtensionsDir = true;
       enableExtensionUpdateCheck = true;
